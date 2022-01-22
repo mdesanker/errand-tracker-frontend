3db4c8191fe47014944cf6b93568e9cac3098852
Michael Desanker <mdesanker@gmail.com>
2022-01-22 06:53:25 -0500

    Set up state to control form data

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,19 +1,52 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { username, email, password, password2 } = formData;
+
+  const formChangeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    // Check passwords match
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Wrapper>
       <Card>
         <h1>Sign up.</h1>
         <p>Create an account</p>
-        <form>
+        <form onSubmit={formSubmitHandler}>
           <input
             type="text"
             name="username"
             id="username"
             placeholder="Username"
             required
+            value={username}
+            onChange={formChangeHandler}
           />
           <input
             type="email"
@@ -21,6 +54,8 @@ const Register = () => {
             id="email"
             placeholder="name@domain.com"
             required
+            value={email}
+            onChange={formChangeHandler}
           />
           <input
             type="password"
@@ -28,6 +63,8 @@ const Register = () => {
             id="password"
             placeholder="Password"
             required
+            value={password}
+            onChange={formChangeHandler}
           />
           <input
             type="password"
@@ -35,6 +72,8 @@ const Register = () => {
             id="password2"
             placeholder="Confirm password"
             required
+            value={password2}
+            onChange={formChangeHandler}
           />
           <button type="submit">Sign up</button>
         </form>
