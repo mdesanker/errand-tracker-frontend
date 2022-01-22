7ce8d5831edf4fc96d24e4214fde053d5b0af7a1
Michael Desanker <mdesanker@gmail.com>
2022-01-22 06:16:43 -0500

    Set up theme provider

---
+++ src/theme/Theme.js
@@ -0,0 +1,30 @@
+const Theme = {
+  colors: {
+    dark: "#1d3557",
+    medium: "#457b9d",
+    light: "#a8dadc",
+    faint: "#f1faee",
+    accent: "#e63946",
+  },
+  heights: {
+    header: "50px",
+    footer: "20px",
+  },
+  widths: {
+    mobile: "max-width: 600px",
+    tablet: "min-width: 600px",
+    computer: "min-width: 992px",
+  },
+  shadows: {
+    sharp:
+      "0 1px 1px rgba(0,0,0,0.25), 0 2px 2px rgba(0,0,0,0.20), 0 4px 4px rgba(0,0,0,0.15), 0 8px 8px rgba(0,0,0,0.10), 0 16px 16px rgba(0,0,0,0.05);",
+    soft: "0 1px 2px rgba(0,0,0,0.07), 0 2px 4px rgba(0,0,0,0.07), 0 4px 8px rgba(0,0,0,0.07), 0 8px 16px rgba(0,0,0,0.07), 0 16px 32px rgba(0,0,0,0.07), 0 32px 64px rgba(0,0,0,0.07);",
+  },
+  radii: {
+    small: "3px",
+    medium: "5px",
+    large: "8px",
+  },
+};
+
+export default Theme;
