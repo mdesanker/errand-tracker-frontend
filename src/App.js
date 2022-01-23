9f48fb89c92cfa526cb72bd4b14997dfe324d7b6
Michael Desanker <mdesanker@gmail.com>
2022-01-22 19:48:42 -0500

    Initialize store

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import GlobalStyles from "./theme/GlobalStyles";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import Header from "./components/header/Header";
 import Dashboard from "./components/dashboard/Dashboard";
 
 const App = () => {
   return (
     <Fragment>
       <GlobalStyles />
-      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
