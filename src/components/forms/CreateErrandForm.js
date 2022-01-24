22894ed5f06fa36d55ada1025f4e8734a8252924
Michael Desanker <mdesanker@gmail.com>
2022-01-24 07:09:22 -0500

    Style form elements

--- src/components/forms/CreateErrandForm.js
+++ src/components/forms/CreateErrandForm.js
@@ -4,31 +4,31 @@ const CreateErrandForm = () => {
   return (
     <Wrapper>
       <Form>
-        <input
+        <Title
           type="text"
           name="title"
           id="title"
-          placeholder="Enter new errand"
+          placeholder="Title (required)"
         />
-        <textarea
+        <Description
           name="description"
           id="description"
           cols="30"
-          rows="10"
-          placeholder="Enter a description"
-        ></textarea>
+          rows="3"
+          placeholder="Description"
+        ></Description>
         <FormGroup>
           <label htmlFor="dueDate">Due date:</label>
-          <input type="date" name="dueDate" id="dueDate" />
+          <Date type="date" name="dueDate" id="dueDate" />
         </FormGroup>
         <FormGroup>
           <label htmlFor="priority">Priority:</label>
-          <select name="priority" id="priority">
+          <PrioritySelect name="priority" id="priority">
             <option value="None">None</option>
             <option value="Low">Low</option>
             <option value="Medium">Medium</option>
             <option value="High">High</option>
-          </select>
+          </PrioritySelect>
         </FormGroup>
         <button>Create errand</button>
       </Form>
@@ -40,15 +40,46 @@ const Wrapper = styled.main`
   width: 100%;
   height: 100vh;
   background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Form = styled.form`
   width: 100%;
-  height: 100%;
+  padding: 1rem;
   display: flex;
   flex-direction: column;
+  gap: 10px;
 `;
 
-const FormGroup = styled.div``;
+const Title = styled.input`
+  font-size: 1rem;
+  padding: 5px;
+  border: none;
+  border-bottom: 1px solid gray;
+`;
+
+const Description = styled.textarea`
+  font-size: 1rem;
+  font-family: inherit;
+  padding: 5px;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const Date = styled.input`
+  font-size: 1rem;
+  padding: 5px;
+`;
+
+const PrioritySelect = styled.select`
+  font-size: 1rem;
+  padding: 5px;
+`;
 
 export default CreateErrandForm;
