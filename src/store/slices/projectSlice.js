5f021f2a8ba7bc4b1314daa65217a282ec0d25d2
Michael Desanker <mdesanker@gmail.com>
2022-01-25 16:00:34 -0500

    Add create project thunk

--- src/store/slices/projectSlice.js
+++ src/store/slices/projectSlice.js
@@ -1,4 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { timedAlert } from "./alertSlice";
+
+export const createProject = createAsyncThunk(
+  "project/createProject",
+  async ({ title, description }, thunkAPI) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = JSON.stringify({ title, description });
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/project/create",
+        body,
+        config
+      );
+      console.log(res.data);
+      return res.data;
+    } catch (err) {
+      const errors = err.response.data.errors;
+      if (errors) {
+        errors.forEach((error) =>
+          thunkAPI.dispatch(timedAlert({ ...error, type: "danger" }))
+        );
+      }
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
 
 const initialState = {
   projects: [],
