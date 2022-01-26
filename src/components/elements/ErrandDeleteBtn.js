6ea89505dfc6ba7cd9ed4d1adf1069f28fcc7a9f
Michael Desanker <mdesanker@gmail.com>
2022-01-26 16:20:12 -0500

    Toggle errand delete btn visibility with mode

--- src/components/elements/ErrandDeleteBtn.js
+++ src/components/elements/ErrandDeleteBtn.js
@@ -1,33 +1,40 @@
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { deleteErrand } from "../../store/slices/errandSlice";
 
 const ErrandDeleteBtn = ({ id }) => {
   const dispatch = useDispatch();
+  const { edit } = useSelector((state) => state.ui);
+
   // console.log(id);
 
   const errandDeleteHandler = () => {
     dispatch(deleteErrand({ id }));
   };
 
-  return <Wrapper onClick={errandDeleteHandler}>Delete</Wrapper>;
+  return (
+    <Wrapper mode={edit} onClick={errandDeleteHandler}>
+      Delete
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.button`
-  // display: none;
+  display: ${({ mode }) => (mode ? "block" : "none")};
   font-size: 12px;
   font-weight: bold;
   text-transform: uppercase;
   padding: 5px;
-  color: lightgray;
-  border: 2px solid lightgray;
+  color: rgba(255, 0, 0, 0.3);
+  border: 2px solid rgba(255, 0, 0, 0.3);
   border-radius: ${({ theme }) => theme.radii.small};
   margin-left: 5px;
   cursor: pointer;
 
   &:hover {
-    color: gray;
-    border: 2px solid gray;
+    color: rgba(255, 0, 0, 0.6);
+    border: 2px solid rgba(255, 0, 0, 0.6);
   }
 `;
 
