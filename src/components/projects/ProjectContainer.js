f0a4f8ad6ed6b84c7c86863e4722a1916b73c558
Michael Desanker <mdesanker@gmail.com>
2022-01-27 13:41:34 -0500

    Filter projects user owns

--- src/components/projects/ProjectContainer.js
+++ src/components/projects/ProjectContainer.js
@@ -5,13 +5,24 @@ import styled from "styled-components";
 const ProjectContainer = () => {
   const dispatch = useDispatch();
 
-  const { project } = useSelector((state) => state.projects);
+  const { projects } = useSelector((state) => state.projects);
 
   const { user } = useSelector((state) => state.user);
+
+  console.log(projects);
+
+  const ownerProjects = projects.filter(
+    (project) => project.author === user._id
+  );
+
+  console.log(ownerProjects);
 
   return (
     <Wrapper>
       <Header>Projects (Owner)</Header>
+      {ownerProjects.map((project) => {
+        return <p key={project._id}>{project.title}</p>;
+      })}
       <Header>Projects (Member)</Header>
     </Wrapper>
   );
