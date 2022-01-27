85222c6a126f11d5b36a3feb4b1d55a5611142b7
Michael Desanker <mdesanker@gmail.com>
2022-01-27 16:48:50 -0500

    Set up form and state in project edit component

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import ExitBtn from "../elements/ExitBtn";
@@ -5,12 +6,47 @@ import ExitBtn from "../elements/ExitBtn";
 const ProjectDetail = () => {
   const { id } = useParams();
 
-  console.log(id);
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const { title, description } = formData;
+
+  const formChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <Wrapper>
       <ExitBtn to="/projects" />
-      Project detail
+      <Form onSubmit={formSubmitHandler}>
+        <Title
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Title (required)"
+          value={title}
+          onChange={formChangeHandler}
+        />
+        <Description
+          name="description"
+          id="description"
+          cols="30"
+          rows="3"
+          placeholder="Description"
+          value={description}
+          onChange={formChangeHandler}
+        ></Description>
+      </Form>
     </Wrapper>
   );
 };
@@ -22,6 +58,27 @@ const Wrapper = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Title = styled.input`
+  font-size: 1rem;
+  padding: 5px;
+  border: none;
+  border-bottom: 1px solid gray;
+`;
+
+const Description = styled.textarea`
+  font-size: 1rem;
+  font-family: inherit;
+  padding: 5px;
 `;
 
 export default ProjectDetail;
