daaae2bc85b887de028f1170cc08c53dee3c4e8e
Michael Desanker <mdesanker@gmail.com>
2022-01-30 05:13:57 -0500

    Add logout btn to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,41 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/slices/userSlice";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <Wrapper>
       <NavList>
         <NavItem>
-          <StyledNavLink to="/">
+          <StyledNavLink to="/dashboard">
             <i className="fas fa-home" />
             <p>Home</p>
           </StyledNavLink>
         </NavItem>
         <NavItem>
-          <StyledNavLink to="/">
+          <StyledNavLink to="/projects">
             <i className="fas fa-folder-open" />
             <p>Projects</p>
           </StyledNavLink>
         </NavItem>
         <NavItem>
-          <StyledNavLink to="/">
+          <StyledNavLink to="/friends">
             <i className="fas fa-users" />
             <p>Friends</p>
           </StyledNavLink>
+        </NavItem>
+        <NavItem>
+          <SignoutBtn onClick={logoutHandler}>
+            <i className="fas fa-sign-out-alt" />
+            <p>Log out</p>
+          </SignoutBtn>
         </NavItem>
       </NavList>
     </Wrapper>
@@ -42,8 +56,16 @@ const Wrapper = styled.nav`
 `;
 
 const NavList = styled.ul`
+  width: 100%;
+  max-width: 500px;
   display: flex;
   align-items: center;
+  justify-content: space-evenly;
+
+  @media only screen and (min-width: 600px) {
+    justify-content: center;
+    gap: 20px;
+  } ;
 `;
 
 const NavItem = styled.li`
@@ -60,11 +82,11 @@ const StyledNavLink = styled(NavLink)`
   color: gray;
   cursor: pointer;
   height: 60px;
-  width: 70px;
-  border-bottom: 3px solid transparent;
+  width: 80px;
+  border: 3px solid transparent;
 
   & i {
-    font-size: 34px;
+    font-size: 28px;
   }
 
   & p {
@@ -73,11 +95,58 @@ const StyledNavLink = styled(NavLink)`
   }
 
   &:hover {
-    color: ${({ theme }) => theme.colors.blue};
+    background-color: #efefef;
+    border: 3px solid white;
+    border-radius: 15px;
   }
 
   &.active {
-    border-bottom: 3px solid ${({ theme }) => theme.colors.blue};
+    color: ${({ theme }) => theme.colors.medium};
+    border-bottom: 3px solid ${({ theme }) => theme.colors.medium};
+  }
+
+  &.active:hover {
+    color: ${({ theme }) => theme.colors.medium};
+    border-bottom: 3px solid ${({ theme }) => theme.colors.medium};
+    background-color: transparent;
+    border-radius: 0;
+  }
+`;
+
+const SignoutBtn = styled.button`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: gray;
+  cursor: pointer;
+  height: 60px;
+  width: 70px;
+  border-bottom: 3px solid transparent;
+  background-color: transparent;
+  border: none;
+
+  & i {
+    font-size: 28px;
+  }
+
+  & p {
+    text-transform: uppercase;
+    font-size: 12px;
+  }
+
+  &:hover {
+    background-color: #efefef;
+    border: 3px solid white;
+    border-radius: 15px;
+  }
+
+  &:active {
+    color: ${({ theme }) => theme.colors.medium};
+    background-color: transparent;
+    border-radius: 0;
   }
 `;
 
