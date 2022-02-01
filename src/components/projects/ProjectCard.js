62373a14491f7f91fd9155d80866c07c663dc23f
Michael Desanker <mdesanker@gmail.com>
2022-02-01 10:55:01 -0500

    Display project author and members on card

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -6,19 +6,24 @@ import ProjectRemoveBtn from "./ProjectRemoveBtn";
 const ProjectCard = ({ project }) => {
   const { _id, title, author, members } = project;
 
+  console.log(author);
+
   const memberString =
     members && members.map((member) => member.username).join(", ");
 
   const { user } = useSelector((state) => state.user);
 
-  const isAuthor = author === user._id;
+  const isAuthor = author._id === user._id;
 
   return (
     <Card>
-      <Title>{title}</Title>
-      <MemberList>{memberString && `[Shared: ${memberString}]`}</MemberList>
-      {isAuthor && <ProjectEditLink id={_id} />}
-      {!isAuthor && <ProjectRemoveBtn id={_id} />}
+      <Section>
+        <Title>{title}</Title>
+        {isAuthor && <ProjectEditLink id={_id} />}
+        {!isAuthor && <ProjectRemoveBtn id={_id} />}
+      </Section>
+      {author && !isAuthor && <InfoText>Author: {author.username}</InfoText>}
+      {memberString && <InfoText>Members: {memberString}</InfoText>}
     </Card>
   );
 };
@@ -29,26 +34,35 @@ const Card = styled.div`
   padding: 8px;
   margin: 0.25rem 0;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
   cursor: default;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Section = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Title = styled.div`
   font-size: 1rem;
-  text-transform: uppercase;
+  font-weight: bold;
   color: black;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 `;
 
-const MemberList = styled.p`
+const InfoText = styled.p`
+  width: 100%;
   color: gray;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
-  padding: 0 8px;
+  padding: 0 10px;
   flex-grow: 1;
 
   @media only screen and (max-width: 650px) {
