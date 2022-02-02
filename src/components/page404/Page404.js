5d0a0b497a86eec1262b2f6a75bea88866dedd95
Michael Desanker <mdesanker@gmail.com>
2022-02-02 07:34:06 -0500

    Style 404 page

--- src/components/page404/Page404.js
+++ src/components/page404/Page404.js
@@ -1,9 +1,50 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 const Page404 = () => {
   return (
     <main>
-      <h1>404</h1>
+      <Wrapper>
+        <Title>
+          4<i className="fas fa-bug" />4
+        </Title>
+        <p>
+          The page you are looking for was not found. Let's try going back to
+          the login page:
+        </p>
+        <StyledLink to="/">Back to safety</StyledLink>
+      </Wrapper>
     </main>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 1rem;
+  text-align: center;
+  gap: 2rem;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  letter-spacing: 0.8rem;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 1rem;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.dark};
+  text-decoration: none;
+  padding: 1rem 2rem;
+  border: 2px solid ${({ theme }) => theme.colors.dark};
+  border-radius: ${({ theme }) => theme.radii.medium};
+
+  &:hover {
+    color: white;
+    background-color: ${({ theme }) => theme.colors.dark};
+  }
+`;
 
 export default Page404;
